8a2dcbbbb2789abeeef3d428cb5e7458a09bdd17
jlzg1987 <jlzg1987@hotmail.com>
2024-12-19 12:40:38 -0500

    update

--- src/pages/toolBar.jsx
+++ src/pages/toolBar.jsx
@@ -4,11 +4,11 @@ function ToolBar() {
     return (
         <>
              <div className="middle">
-            <a href="#inicio" className="btn btn0"><i class='bx bxs-home' ></i></a>
-            <a href="#aboutMe" className="btn btn1"><i class='bx bxs-user' ></i></a>
-            <a href="#habilidades" className="btn btn2"><i class='bx bx-chip' ></i></a>
-            <a href="#PortafolioA" className="btn btn3"><i class='bx bxs-folder-open' ></i></a>
-            <a href="#contacto" className="btn btn4"><i class='bx bxs-contact'></i></a>
+            <a  href="#inicio" className="  btn btn0"><i class='bx bxs-home' ></i> <span className='tool'>Home</span></a>
+            <a href="#aboutMe" className="btn btn1"><i class='bx bxs-user' ></i><span className='tool'>Sobre mí</span></a>
+            <a href="#habilidades" className="btn btn2"><i class='bx bx-chip' ></i><span className='tool'>Habilidades</span></a>
+            <a href="#PortafolioA" className="btn btn3"><i class='bx bxs-folder-open' ></i><span className='tool'>Portafolio</span></a>
+            <a href="#contacto" className="btn btn4"><i class='bx bxs-contact'></i><span className='tool'>Contacto</span></a>
            
 
         </div>
