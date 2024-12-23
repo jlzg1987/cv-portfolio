401b824898a8e7a3511ad85385f097fc81be050c
jlzg1987 <jlzg1987@hotmail.com>
2024-12-23 11:43:29 -0500

    up

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,15 @@ function App() {
  
 
         <BarMainPager />
-        
+        <SolLuna/>
+        <ToolBar/>
+        <Principal />
+        <AbautMe />
+        <Habilidades />
+        <Portafolio />
+        <Telecomunicaciones/>
+        <Contacto />
+        <Piepagina/>
 
       </div>
 
